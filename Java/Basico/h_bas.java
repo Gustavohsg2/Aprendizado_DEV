d02e235173f5e455c4835d1466540b0adaf36b07
Gustavo Henrique <gustavohsg2@hotmail.com>
2023-11-12 18:13:55 -0300

    Estudos JS

---
+++ Java/Basico/h_bas.java
@@ -0,0 +1,10 @@
+package Java.Basico;
+import java.util.Scanner;
+
+public class h_bas {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
