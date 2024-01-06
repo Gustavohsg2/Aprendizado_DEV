951993c74d73dc0728bf8d7c27518405959366d0
Gustavo Henrique <gustavohsg2@hotmail.com>
2024-01-05 22:46:02 -0300

    coisas

---
+++ JavaScript/JS IFRS/Eventos/Load/load.js
@@ -0,0 +1,15 @@
+//Eventos
+
+//Load - Carregamento
+
+// onLoad: quando a página ou imagem é carregada.
+// onResize: quando a janela do navegador é redimensionada.
+
+
+function carreguei() {
+    document.getElementById('log').innerHTML += "carreguei<br>";
+}
+
+function redimensionei() {
+    document.getElementById('log').innerHTML += "redimensionei<br>";
+}
